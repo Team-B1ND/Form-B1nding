323d5d4144727bbf51f12cdac83bb05db3516315
임동현 <79711744+ldh3907@users.noreply.github.com>
2023-01-06 17:14:43 +0900

    📑 ::reset css 파일 추가, ⚙️ ::esbuild css번들 설정

--- packages/b1nd-design-core/esbuild.config.js
+++ packages/b1nd-design-core/esbuild.config.js
@@ -1,13 +1,18 @@
 const { build } = require("esbuild");
-const pkg = require("./package.json");
 const { pnpPlugin } = require("@yarnpkg/esbuild-plugin-pnp");
+const cssModulesPlugin = require("esbuild-css-modules-plugin");
 
 build({
-  entryPoints: ["./src/index.ts"],
-  outfile: pkg.main,
+  entryPoints: ["./src/index.ts", "./src/b1ndResetStyle.css"],
+  outdir: "./dist",
   format: "cjs",
   platform: "node",
   sourcemap: true,
   bundle: true,
-  plugins: [pnpPlugin()],
+  plugins: [
+    pnpPlugin(),
+    cssModulesPlugin({
+      filter: /\.css$/,
+    }),
+  ],
 }).catch(() => process.exit(1));
